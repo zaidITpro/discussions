45471a54e7201aac221d58825861da0b7558bcec
zaidITpro <zaid.khan@daffodilsw.com>
2018-12-01 17:46:03 +0530

    login module

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def login(reqeust):
+    return HttpResponse('This is going to be our login page ! Got it ?')
