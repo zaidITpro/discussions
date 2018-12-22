5e42226b1cad8801d5f9e285c3c11b2f81d64e7b
zaidITpro <zaid.khan@daffodilsw.com>
2018-12-22 22:39:41 +0530

    deleted login and signup app

--- login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def login(reqeust):
-    return HttpResponse('This is going to be our login page ! Got it ?')
--- signup/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def signup(reqeust):
-    return HttpResponse('This is going to be our signup page ! Got it ?')
