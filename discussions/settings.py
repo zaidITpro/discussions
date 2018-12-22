6cce3a42e6ac7d46c774947d6fed15f5664dd507
zaidITpro <zaid.khan@daffodilsw.com>
2018-12-22 23:30:20 +0530

    users module added

--- discussions/settings.py
+++ discussions/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',  #custom app
+    'user',  #custom app
     
 ]
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'discussions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
