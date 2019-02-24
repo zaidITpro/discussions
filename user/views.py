0ac546b548c88fc657444a373c4f834c1c5708df
zaidITpro <zaid.khan@daffodilsw.com>
2019-02-24 20:47:58 +0530

    singup and login page added

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    return render(request,'app/discussions/signup-simple.html')
+
+def signin(request):
+    return render(request,'app/discussions/signin-simple.html')
